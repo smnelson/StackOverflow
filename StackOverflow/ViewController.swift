65c5d73042ab2eabacb05b3777d8b76d405e121b
Stacey Nelson <smnelson15@gmail.com>
2018-12-22 10:19:32 -0600

    Updates/Addition: - Updated Label - Added user ID field - Added password field - Added constraints to new user id's and passwords to format on screen

--- StackOverflow/ViewController.swift
+++ StackOverflow/ViewController.swift
@@ -12,6 +12,14 @@ import SnapKit
 class ViewController: UIViewController {
     
     let primaryLabel = UILabel(frame: .zero)
+    let userID = UITextField(frame: .zero)
+    let passwordInput = UITextField(frame: .zero)
+    //let textFieldWidth = 200
+    let submitButton = UIButton(type: .custom)
+    let submitButtonColor = UIColor(displayP3Red: 17 / 255.0,
+                                    green: 104 / 255.0,
+                                    blue: 30 / 255.0,
+                                    alpha: 1.0)
     
     init() {
         super.init(nibName: nil, bundle: nil)
@@ -26,14 +34,29 @@ class ViewController: UIViewController {
     
     func updateSubviews() {
         view.addSubview(primaryLabel)
+        view.addSubview(userID)
+        view.addSubview(passwordInput)
+        view.addSubview(submitButton)
     }
     
     func setupSubviews() {
         view.backgroundColor = .white
         
-        primaryLabel.text = "Emergency Alert Text!"
+        primaryLabel.text = "StackOverflow"
         primaryLabel.font = UIFont.systemFont(ofSize: 24.0, weight: .bold)
         primaryLabel.textColor = .red
+        
+        userID.borderStyle = .roundedRect
+        passwordInput.borderStyle = .roundedRect
+        
+        userID.placeholder = "Username"
+        passwordInput.placeholder = "Password"
+        
+        submitButton.layer.cornerRadius = 15.0
+        submitButton.backgroundColor = submitButtonColor
+        submitButton.setTitle("Submit", for: .normal)
+        submitButton.setTitleColor(.white, for: .normal)
+        submitButton.addTarget(self, action: #selector(ViewController.submitButtonPressed), for: UIControl.Event.touchUpInside)
     }
     
     func setupConstraints() {
@@ -41,6 +64,34 @@ class ViewController: UIViewController {
             maker.centerX.equalToSuperview()
             maker.centerY.equalToSuperview().multipliedBy(0.5)
         }
+        
+        userID.snp.makeConstraints { field in
+            //field.centerX.equalToSuperview()
+            field.top.equalTo(primaryLabel.snp.bottom).offset(60)
+            //field.width.equalTo(textFieldWidth)
+            field.leading.equalTo(view.safeAreaLayoutGuide.snp.leading).offset(90)
+            field.trailing.equalTo(view.safeAreaLayoutGuide.snp.trailing).inset(90)
+        }
+        
+        passwordInput.snp.makeConstraints { field in
+            //field.centerX.equalToSuperview()
+            field.top.equalTo(userID.snp.bottom).offset(8)
+            //field.centerY.equalToSuperview().multipliedBy(0.8)
+            //field.width.equalTo(textFieldWidth)
+            field.leading.equalTo(userID.snp.leading)
+            field.trailing.equalTo(userID.snp.trailing)
+        }
+        
+        submitButton.snp.makeConstraints { button in
+            button.top.equalTo(passwordInput.snp.bottom).offset(8)
+            button.leading.equalTo(passwordInput.snp.leading)
+            button.trailing.equalTo(passwordInput.snp.trailing)
+        }
+    }
+    
+    @objc
+    func submitButtonPressed() {
+        print("Button pressed")
     }
 }
 
