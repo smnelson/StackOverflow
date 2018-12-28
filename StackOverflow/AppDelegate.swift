9f192684267e9e092a8cfe862b99a71cdc411b51
Stacey Nelson <smnelson15@gmail.com>
2018-12-27 19:10:49 -0600

    Changed ViewController name to LoginViewController

--- StackOverflow/AppDelegate.swift
+++ StackOverflow/AppDelegate.swift
@@ -16,7 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        let mainViewController = ViewController()
+        let mainViewController = LoginViewController()
         
         window?.rootViewController = mainViewController
         window?.makeKeyAndVisible()
